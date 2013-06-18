d36f4d2919a19051afa95e3f83584747efb23cd7
ghost77 <robert.g.77@wp.pl>
2013-06-18 22:22:40 +0200

    try

---
+++ src/main/java/org/kornicameister/sise/lake/types/actors/impl/rg/AnglerActorRG.java
@@ -0,0 +1,28 @@
+package org.kornicameister.sise.lake.types.actors.impl.rg;
+
+import org.kornicameister.sise.lake.types.actors.DefaultActor;
+import org.kornicameister.sise.lake.types.actors.LakeActors;
+
+public class AnglerActorRG extends DefaultActor {
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String getFactName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected LakeActors setType() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	
+	
+}
---
+++ src/main/java/org/kornicameister/sise/lake/types/actors/impl/rg/HerbivoreFishActorRG.java
@@ -0,0 +1,26 @@
+package org.kornicameister.sise.lake.types.actors.impl.rg;
+
+import org.kornicameister.sise.lake.types.actors.DefaultActor;
+import org.kornicameister.sise.lake.types.actors.LakeActors;
+
+public class HerbivoreFishActorRG extends DefaultActor {
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String getFactName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected LakeActors setType() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
