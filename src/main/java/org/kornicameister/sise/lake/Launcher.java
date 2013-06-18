8030727e25028782f86666b875a7812b898cb826
Karol Górecki <karolgurecki@gmail.com>
2013-06-18 17:16:40 +0200

    ui kinda works!

--- src/main/java/org/kornicameister/sise/lake/Launcher.java
+++ src/main/java/org/kornicameister/sise/lake/Launcher.java
@@ -9,25 +9,24 @@ import java.util.Scanner;
 public class Launcher {
 
     public static void main(String[] args) {
-        ClispEnvironment clispEnvironment = ClispEnvironment.getInstance(args[0]);
-        Scanner scanIn = new Scanner(System.in);
+        final String clispEnvironment = args[0];
         final String uiPropFile = args[1];
         EventQueue.invokeLater(new Runnable() {
             @Override
             public void run() {
-                new WorldUI(uiPropFile);
+                new WorldUI(clispEnvironment, uiPropFile);
             }
         });
-        if (clispEnvironment.isBootstrapped()) {
-            boolean nextLoop = true;
-            while (nextLoop) {
-                clispEnvironment.mainLoop();
-                System.out.print("Next tour -> \t");
-                String yes_no = scanIn.next();
-                nextLoop = yes_no.equalsIgnoreCase("yes");
-            }
-            scanIn.close();
-            clispEnvironment.destroy();
-        }
+//        if (clispEnvironment.isBootstrapped()) {
+//            boolean nextLoop = true;
+//            while (nextLoop) {
+//                clispEnvironment.mainLoop();
+//                System.out.print("Next tour -> \t");
+//                String yes_no = scanIn.next();
+//                nextLoop = yes_no.equalsIgnoreCase("yes");
+//            }
+//            scanIn.close();
+//            clispEnvironment.destroy();
+//        }
     }
 }
