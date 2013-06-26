e4003fe8fd669d854c4702d7f9d745a90c09095a
tszopka <tomasz.szopka@gmail.com>
2013-06-26 18:45:22 +0200

    Corrected my impl + description in report

--- src/main/java/org/kornicameister/sise/lake/types/actors/impl/ts/PoacherActorTS.java
+++ src/main/java/org/kornicameister/sise/lake/types/actors/impl/ts/PoacherActorTS.java
@@ -1,7 +1,11 @@
 package org.kornicameister.sise.lake.types.actors.impl.ts;
 
+import org.kornicameister.sise.lake.adapters.BooleanToSymbol;
+import org.kornicameister.sise.lake.types.WorldHelper;
 import org.kornicameister.sise.lake.types.actors.DefaultActor;
 import org.kornicameister.sise.lake.types.actors.LakeActors;
+
+import CLIPSJNI.PrimitiveValue;
 
 public class PoacherActorTS extends DefaultActor {
 	
@@ -22,4 +26,19 @@ public class PoacherActorTS extends DefaultActor {
 	public String getFactName() {
 		return PoacherActorTS.class.getSimpleName();
 		}
+	
+	public void applyFact(PrimitiveValue value) throws Exception {
+        this.setAtField(WorldHelper.getField(value.getFactSlot("atField").intValue()));
+        this.setAttackPower(value.getFactSlot("attackPower").intValue());
+        this.setAttackRange(value.getFactSlot("attackRange").intValue());
+        this.setMoveRange(value.getFactSlot("moveRange").intValue());
+        this.setCash(value.getFactSlot("cash").intValue());
+        this.setHp(value.getFactSlot("hp").intValue());
+        this.setTargetHit(BooleanToSymbol.fromSymbol(value.getFactSlot("targetHit").symbolValue()));
+        this.setHunger(value.getFactSlot("hunger").intValue());
+        this.setWeight(value.getFactSlot("weight").intValue());
+        this.setHowManyFishes(value.getFactSlot("howManyFishes").intValue());
+        this.setTookBribe(BooleanToSymbol.fromSymbol(value.getFactSlot("tookBribe").symbolValue()));
+        this.setCorruptionThreshold(value.getFactSlot("corruptionThreshold").intValue());
+    }
 }
