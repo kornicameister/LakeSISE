f5359822d4fb15b69102a5cc8ef514351d545fed
Tomasz Trębski <kornicameister@gmail.com>
2013-09-09 21:52:15 +0200

    b

---
+++ src/main/java/org/kornicameister/sise/lake/types/ClispAsserted.java
@@ -0,0 +1,15 @@
+package org.kornicameister.sise.lake.types;
+
+import java.lang.annotation.*;
+
+/**
+ * @author kornicameister
+ * @version 0.0.1
+ * @since 0.0.1
+ */
+@Target({ElementType.FIELD})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface ClispAsserted {
+
+}
