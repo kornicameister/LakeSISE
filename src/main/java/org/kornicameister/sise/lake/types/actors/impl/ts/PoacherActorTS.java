56bc3c73cdf3ce833b28f9dfdde62d7c480e3055
tszopka <tomasz.szopka@gmail.com>
2013-09-10 22:12:45 +0200

    My latest impl.

--- src/main/java/org/kornicameister/sise/lake/types/actors/impl/ts/PoacherActorTS.java
+++ src/main/java/org/kornicameister/sise/lake/types/actors/impl/ts/PoacherActorTS.java
@@ -1,14 +1,22 @@
 package org.kornicameister.sise.lake.types.actors.impl.ts;
 
+import java.util.Set;
+
+import org.apache.log4j.Logger;
 import org.kornicameister.sise.lake.adapters.BooleanToSymbol;
 import org.kornicameister.sise.lake.types.WorldHelper;
 import org.kornicameister.sise.lake.types.actors.DefaultActor;
 import org.kornicameister.sise.lake.types.actors.LakeActors;
+import org.kornicameister.sise.lake.types.actors.impl.ts.PoacherActorTS;
+import org.kornicameister.sise.lake.types.effectiveness.EffectivenessConstants;
+import org.kornicameister.sise.lake.types.effectiveness.EffectivenessResult;
+
+import com.google.common.collect.Sets;
 
 import CLIPSJNI.PrimitiveValue;
 
 public class PoacherActorTS extends DefaultActor {
-	
+	private static final Logger LOGGER = Logger.getLogger(PoacherActorTS.class);
 	public PoacherActorTS () {
 		super();
 		}
@@ -18,6 +26,28 @@ public class PoacherActorTS extends DefaultActor {
 		return LakeActors.POACHER;
 		}
 		
+	@Override
+	public void applyEffectiveness(PrimitiveValue value) throws Exception {
+		try {
+            this.effectivity_1 += value.getFactSlot(EffectivenessConstants.FieldsNames.EFF_1).doubleValue();
+            this.effectivity_2 += value.getFactSlot(EffectivenessConstants.FieldsNames.EFF_2).doubleValue();
+        } catch (Exception exception) {
+            LOGGER.warn(String
+                    .format("Error occurred when resolving effectiveness from primitive_value = %s", value), exception);
+        }
+	}
+
+	@Override
+	public Set<EffectivenessResult> getEffectiveness() {
+		final Set<EffectivenessResult> results = Sets.newHashSet();
+
+        results.add(new EffectivenessResult<>(EffectivenessConstants.Effectiveness.EFF_TOTAL_TICKETS, this.effectivity_1));
+        results.add(new EffectivenessResult<>(EffectivenessConstants.Effectiveness.EFF_CAUGHT_FISHES, this.effectivity_2 / this
+                .getRoundsAlive()));
+
+        return results;
+	}
+
 	@Override
 	public void run() {
 		}
